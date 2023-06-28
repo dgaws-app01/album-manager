0bb2ad389e3558f7295d60d3a1a107e138610cf1
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2023-06-28 08:50:06 -0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import "./style.css";
 import {TodoTestList} from "./components/TodoTestList.js"
 
 export default function App() {
-  try{
-    let f = fetch("https://script.google.com/macros/s/AKfycbz-sN9HyNIDWW0hPnaZlfZOFmsXF8M7y_4oq68iDucPDIVonUIbIws_7vu_x2t5zZE75g/exec", {method:"POST"})
-    f.then(r=> r.json().then(j=> console.log(j)))
-  }catch(e){
-    console.log(e)
-  }
+  
   return (
     <div>
       <h1>Album manager .</h1>
--- src/components/TodoTestList.js
+++ src/components/TodoTestList.js
@@ -1,39 +1,54 @@
-import React from "react";
-import {useState} from "react"
-import {useDispatch, useSelector} from "react-redux"
-import {AddTodoAction} from "../actions/TodoTestActions.js"
+import React from 'react';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AddTodoAction } from '../actions/TodoTestActions.js';
 
 export const TodoTestList = (props) => {
-  const [td, setTd] = useState()
-  const dispatch = useDispatch()
-  const Todo = useSelector((state) => state.Todo)
-  const {todos} = Todo || {}
-  const {nm} = props
+  const [td, setTd] = useState();
+  const dispatch = useDispatch();
+  const Todo = useSelector((state) => state.Todo);
+  const { todos } = Todo || {};
+  const { nm } = props;
 
   const addNewItem = (e) => {
-    e.preventDefault()    
-    dispatch(AddTodoAction(td))
-    
-  }
+    e.preventDefault();
+    dispatch(AddTodoAction(td));
+    try {
+      let f = fetch(
+        'https://script.google.com/macros/s/AKfycbz-sN9HyNIDWW0hPnaZlfZOFmsXF8M7y_4oq68iDucPDIVonUIbIws_7vu_x2t5zZE75g/exec',
+        { method: 'POST' }
+      );
+      f.then((r) => r.json().then((j) => console.log(j)));
+    } catch (er) {
+      console.log(er);
+    }
+  };
 
   //console.log({td, setTd, useDispatch, dispatch, Todo})
-  console.log({time : new Date().getTime(), reloaded : nm})
-    
+  console.log({ time: new Date().getTime(), reloaded: nm });
+
   return (
     <>
       <div>
         <div>
-          <label for="#todonew">Add a new item </label> 
-          <input id="todonew" type="text" onChange={(e)=> setTd(e.target.value)}></input><span> </span>          
-          <button onClick={(e)=> addNewItem(e)}>ok</button>
+          <label for="#todonew">Add a new item </label>
+          <input
+            id="todonew"
+            type="text"
+            onChange={(e) => setTd(e.target.value)}
+          ></input>
+          <span> </span>
+          <button onClick={(e) => addNewItem(e)}>ok</button>
         </div>
         <ul>
-          {todos && todos.map(t=> 
-              <li key={t.id}>-#{t.todo}#-<button>x</button></li>)
-          }
-          
+          {todos &&
+            todos.map((t) => (
+              <li key={t.id}>
+                -#{t.todo}#-<button>x</button>
+              </li>
+            ))}
         </ul>
       </div>
     </>
-  )
-}
+  );
+};
