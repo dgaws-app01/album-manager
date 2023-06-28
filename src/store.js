f6d047c38a88e6216af876ff6092fafe05da892a
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2023-06-28 12:32:56 +0530

    1 file modified

--- src/store.js
+++ src/store.js
@@ -1,2 +1,13 @@
 import {legacy_createStore, combineReducers, applyMiddleware} from "redux"
 import thunk from "react-thunk"
+
+const reducer = combineReducers({
+
+})
+
+const initState = {}
+
+const middleware = [thunk]
+
+export const store = legacy_createStore(reducer, initState, applyMiddleware(...middleware) )
+
