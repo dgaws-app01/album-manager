8d22ed9384df8da8df58965d44eb424900550dde
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2023-06-29 09:45:39 +0530

    1 file modified

--- src/components/AttachReducerLater.js
+++ src/components/AttachReducerLater.js
@@ -1,4 +1,6 @@
 import React from "react";import React from "react";
+import {reducer} from "../store"
 export const AttachReducerLater = (params) => {
+  console.log(reducer)
   return <>Attaching later ...</>
 }
