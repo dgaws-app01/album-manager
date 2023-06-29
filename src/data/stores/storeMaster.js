79d8ea3b388e872075af96526263bd39ed46a986
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2023-06-29 18:41:38 +0530

    1 file modified

--- src/data/stores/storeMaster.js
+++ src/data/stores/storeMaster.js
@@ -6,10 +6,24 @@ import {  } from 'redux';
 // Stores 
 const stores = []
 export const defineStore = (props) => {
-  const name = [Object.keys(props)[0]]
+  const name = Object.keys(props)[0]
   if(name){
-    const {[Object.keys(props)[0]]: {initialState, actions}} = props
-    
+    const {[name]: {initialState, actions}} = props
+    const slice = createSlice({
+      name, 
+      initialState, 
+      reducers : {},
+      extraReducers : (builder) => {
+        Object.keys(actions).forEach(action => {
+          if(action.includes("*")){
+            
+          }
+          else{
+            builder.addCase(createAction(action), actions[action])
+          }
+        })
+      }
+    })
   } 
   
   
