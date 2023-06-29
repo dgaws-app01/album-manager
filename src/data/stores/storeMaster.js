cbe352eac601ac3607e14143ff2227697af62c43
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2023-06-29 19:51:30 +0530

    1 file modified

--- src/data/stores/storeMaster.js
+++ src/data/stores/storeMaster.js
@@ -1,14 +1,23 @@
 import { configureStore, createAction, createReducer, createSlice } from '@reduxjs/toolkit'
+import { applyMiddleware } from 'redux';
 import thunk from "redux-thunk"
 import {titleCase} from "../../utils/text"
 
-import {  } from 'redux';
+
 
 // Stores 
-const stores = []
-export const defineStore = (props) => {
-  const name = Object.keys(props)[0]
-  if(name){
+const stores = {
+  master : configureStore({
+    reducer : {},
+    //middleware: (getDefaultMiddleware) => getDefaultMiddleware,
+    preloadedState : {}
+  })
+}
+export const modifyStore = (props) => {  
+  const storeName = Object.keys(props)[0]
+  const store = props[storeName]
+  if(storeName){
+    const name = Object.keys(store)[0]
     const {[name]: {initialState, actions}} = props
     const slice = createSlice({
       name, 
@@ -17,21 +26,29 @@ export const defineStore = (props) => {
       extraReducers : (builder) => {
         Object.keys(actions).forEach(action => {
           if(action.includes("*")){
-            let actNm = action.replace("*", "")
-            let actDef = {
-              [`actNm`] : (action) => {
-                return action.type.includes(actNm)
+            let matcher = action.replace("*", "")
+            let actNm = `is${titleCase(matcher)}`
+            let actMatcher = {
+              [actNm] : (action) => {
+                return action.type.includes(matcher)
               }
             }
-            builder.addMatcher()
+            let actDef = actions[action]
+            builder.addMatcher(actMatcher[actNm], actDef)
           }
           else{
-            builder.addCase(createAction(action), actions[action])
+            let actDef = actions[action]
+            builder.addCase(createAction(action), actDef)
           }
         })
       }
     })
+
+    console.log(slice)
+
   } 
+
+  
   
   
 
